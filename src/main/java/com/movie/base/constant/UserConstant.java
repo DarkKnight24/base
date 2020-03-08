4e00e47c37fcc672eb286f9ca0380bb089a33282
Knight <5324312+knight58@user.noreply.gitee.com>
2020-03-08 11:38:31 +0800

    完善部分功能

--- src/main/java/com/movie/base/constant/UserConstant.java
+++ src/main/java/com/movie/base/constant/UserConstant.java
@@ -7,6 +7,6 @@ import org.springframework.stereotype.Component;
  */
 @Component
 public class UserConstant {
-    public static final String DEFAULT_IMAGE = "group1/M00/00/00/rBF4FV5d1LWAGhl3AAAFgWYz8g0765.jpg";
+    public static final String DEFAULT_IMAGE = "group1/M00/00/00/rBF4FV5jeMSAFjJ6AAAFgWYz8g0482.jpg";
     public static final Integer ROLE = 0;
 }
