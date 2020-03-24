5203ed6db07021e2338ddf3c7b36fe51909aa8dc
huahua <17638351213@163.com>
2020-03-24 15:42:27 +0800

    修改分页属性

--- src/main/java/com/movie/base/utils/Page.java
+++ src/main/java/com/movie/base/utils/Page.java
@@ -1,30 +1,36 @@
 package com.movie.base.utils;
 
-import com.github.pagehelper.IPage;
-import lombok.Data;
-
 import java.util.List;
+
+import com.github.pagehelper.IPage;
+
+import lombok.Data;
 
 @Data
 public class Page<T> implements IPage {
-
-    private Integer pageNum = 1;
+    
+    private Integer currentPage = 1;
+    
     private Integer pageSize = 10;
+    
     private long total;
+    
     private int pages;
+    
     private String orderBy;
+    
     private List<T> list;
-
+    
     @Override
     public Integer getPageNum() {
-        return this.pageNum;
+        return this.currentPage;
     }
-
+    
     @Override
     public Integer getPageSize() {
         return this.pageSize;
     }
-
+    
     @Override
     public String getOrderBy() {
         return this.orderBy;
