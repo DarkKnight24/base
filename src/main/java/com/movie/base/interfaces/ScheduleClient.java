bc9d65bd466f74f74c4ba14bb404bb9564a24f20
huahua <17638351213@163.com>
2020-05-06 16:58:46 +0800

    调整

--- src/main/java/com/movie/base/interfaces/ScheduleClient.java
+++ src/main/java/com/movie/base/interfaces/ScheduleClient.java
@@ -4,7 +4,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient("MovieBaseServer")
+@FeignClient("MovieDataServer")
 public interface ScheduleClient {
     
     @GetMapping("/schedule/detail/{scheduleId}")
